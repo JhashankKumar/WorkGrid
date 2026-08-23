8003a6f6a7fdf24a50663c9f77f94b08223e31aa
jhashank <jhashank@nuevesolutions.in>
2026-08-23 14:39:08 +0000

    initial setup completed

--- client/src/app/layout.tsx
+++ client/src/app/layout.tsx
@@ -1,16 +1,5 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
-import "./globals.css";
 
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
-});
 
 export const metadata: Metadata = {
   title: "Create Next App",
@@ -21,8 +10,7 @@ export default function RootLayout({ children }: LayoutProps<"/">) {
   return (
     <html
       lang="en"
-      className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}
-    >
+      className="bg-zinc-50 font-sans text-zinc-900 antialiased dark:bg-black dark:text-zinc-50">
       <body className="min-h-full flex flex-col">{children}</body>
     </html>
   );
